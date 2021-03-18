51b7b85eeafcb4a5649f520131a3c080c1626da5
Cristhiam Gabriel <cristhiangf98@gmail.com>
2021-03-17 23:13:41 -0500

    Corrige error de configuración de despliegue.

--- configure.sh
+++ configure.sh
@@ -3,5 +3,5 @@ sudo systemctl enable gunicorn
 sudo systemctl start gunicorn
 
 sudo rm /etc/nginx/sites-enabled/default
-sudo mv ./default.conf /etc/nginx/sites-enabled/
+sudo mv ./default /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
